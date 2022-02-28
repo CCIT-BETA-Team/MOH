a1bf341c509b81c02ce169c2d3880c06f724e42a
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2022-02-28 15:10:50 +0900

    22/022/28 플레이어 추가 -전-

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,14 +12,28 @@ public class Player : MonoBehaviour
     public static readonly int Platform = 1;
 #endif
 
+    [Header("플레이어 스테이터스")]
+    int health;
+    float walkingSpeed;
+    float runningSpeed;
+    float unlockSpeed;
+    float noiseValue;
+
+    [Space]
+    [Header("플레이어 아이템 관련")]
+    bool isHoldingItem;
+    bool getQuestItem;
+    enum currentHands { rightHand, leftHand}
+    currentHands currentHand;
+
     void Start()
     {
         switch(Platform)
         {
-            case 0:
+            case 0: //오큘러스
                 Debug.Log("Android Hi~");
                 break;
-            case 1:
+            case 1: //PC
                 Debug.Log("Window Hi~");
                 break;
         }
