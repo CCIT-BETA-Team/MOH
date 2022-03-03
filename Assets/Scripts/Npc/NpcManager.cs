2bbba7317275c0d8815ab05645d7b1393e9ad486
Kang <dkdlfhqht7895@naver.com>
2022-03-03 13:40:29 +0900

    2022/03/03 npc작업 중간 -강-

--- Assets/Scripts/Npc/NpcManager.cs
+++ Assets/Scripts/Npc/NpcManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NpcManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Npc;//리스폰 해줄 Npc
 
-    // Update is called once per frame
-    void Update()
+    public List<Transform> Map_Transform = new List<Transform>();
+
+    private void Awake()
     {
-        
+        Respawn_Npc();
+    }
+    public Transform request() { return this.transform; }
+    //오버로딩으로 경찰스폰이랑 일반 npc스폰 다르게
+
+    public Transform request(Npc.State state)
+    {
+        switch(state)
+        {
+            case global::Npc.State.HUNGRY:
+                //return Map_Transform[0];
+                break;
+        }
+        return null;
+    }
+   
+
+    void Respawn_Npc()
+    {
+        //Npc 리스폰
     }
 }
