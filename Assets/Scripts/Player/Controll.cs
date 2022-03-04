59f93d4e72ae068e411057be6940b27fdf7429fa
DESKTOP-D1GR4R1\ndkch <ndkchan98@gmail.com>
2022-03-04 13:37:12 +0900

    Environment updated

--- Assets/Scripts/Player/Controll.cs
+++ Assets/Scripts/Player/Controll.cs
@@ -7,34 +7,36 @@ public class Controll : MonoBehaviour
 
 
 
-    
+
     public GameObject Lighting;
     public Animator LeftHandAni;
     public Animator RightHandAni;
     public float speed;
 
+    private CharacterController chacontroll;
+
     void Start()
     {
-       
-    var inputDevices = new List<UnityEngine.XR.InputDevice>();
-    UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+        chacontroll = GetComponent<CharacterController>();
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
 
-    foreach (var device in inputDevices)
-    {
-        Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
     }
-}
 
     // Update is called once per frame
     void Update()
     {
-      if(OVRInput.GetDown(OVRInput.Button.One))
-      {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
             Debug.Log("Click one button");
-      }
+        }
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
-            if(Lighting.activeSelf)
+            if (Lighting.activeSelf)
             {
                 Lighting.SetActive(false);
 
@@ -45,20 +47,21 @@ public class Controll : MonoBehaviour
             }
             Debug.Log("Click one button");
         }
-        if(OVRInput.GetDown(OVRInput.Button.Two))
+        if (OVRInput.GetDown(OVRInput.Button.Two))
         {
             RightHandAni.SetTrigger("FY");
         }
-     
+
         if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
         {
             Vector2 PrimaryThumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
-            transform.Translate(PrimaryThumbstick * transform.forward);
-        }
-        if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))
-        {
+            Vector3 Dir = transform.forward * PrimaryThumbstick.y*speed +transform.right*PrimaryThumbstick.x *speed;
+            chacontroll.Move(Dir);
+            if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))
+            {
 
+            }
         }
     }
 }
