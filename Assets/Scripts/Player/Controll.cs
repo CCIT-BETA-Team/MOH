d911a6261fde5e1a54c5474e0e4039ec5140113e
DESKTOP-D1GR4R1\ndkch <ndkchan98@gmail.com>
2022-03-04 17:29:10 +0900

    VR Door Check

--- Assets/Scripts/Player/Controll.cs
+++ Assets/Scripts/Player/Controll.cs
@@ -11,6 +11,9 @@ public class Controll : MonoBehaviour
     public GameObject Lighting;
     public Animator LeftHandAni;
     public Animator RightHandAni;
+
+    public GameObject l_hand;
+    public GameObject r_hand;
     public float speed;
 
     private CharacterController chacontroll;
@@ -49,19 +52,43 @@ public class Controll : MonoBehaviour
         }
         if (OVRInput.GetDown(OVRInput.Button.Two))
         {
-            RightHandAni.SetTrigger("FY");
+           
         }
-
+        Vector3 prevector = new Vector3(0,0,0);
+        GameObject door=null;
+        if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger)!=0)
+        {
+            RaycastHit hit;
+            Physics.Raycast(l_hand.transform.position, l_hand.transform.forward, out hit, 20);
+            if (hit.transform.GetComponent<Door>())
+            {
+                door = hit.transform.gameObject;
+                prevector = l_hand.transform.position;
+                Debug.Log("Object is : " + hit.transform.name);
+                l_hand.transform.position = hit.transform.position;
+            }
+        }
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+          if(door!=null)
+          {
+                l_hand.transform.position = door.transform.position;
+            }
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            door = null;
+            l_hand.transform.position = prevector;
+          
+        }
         if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
         {
             Vector2 PrimaryThumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
             Vector3 Dir = transform.forward * PrimaryThumbstick.y*speed +transform.right*PrimaryThumbstick.x *speed;
-            chacontroll.Move(Dir);
-            if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))
-            {
-
-            }
+            chacontroll.Move(Dir);  
+           
+            
         }
     }
 }
