241750b8a4b710609e6cdf918e9c4d0d0c13acb4
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2022-04-01 18:37:43 +0900

    준비 화면 UI 및 가구 세팅 -전-

---
+++ Assets/Scripts/PreparationSite/Mission/WhiteBoard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhiteBoard : MonoBehaviour
+{
+    public Text mission_name;
+    public Item goal;
+    //보상
+    //의뢰주 이름?
+    public Image mission_image;
+    public Image item_image;
+    public Text coment;
+    public Text scenario;
+    public Text main_mission;
+    public Text sub_mission;
+}
