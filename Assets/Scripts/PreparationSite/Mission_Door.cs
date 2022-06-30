553fc6d6c4630ba2984f647e0ea54097edf3b8fd
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2022-07-01 00:09:40 +0900

    no message

--- Assets/Scripts/PreparationSite/Mission_Door.cs
+++ Assets/Scripts/PreparationSite/Mission_Door.cs
@@ -19,6 +19,6 @@ public class Mission_Door : Item
     
     static void Mission_Start(Mission select_mission)
     {
-        ScenesManager.instance.Load_Scene(select_mission.mission_scene);
+        ScenesManager.instance.Load_Scene(select_mission.mission_scene,ScenesManager.LoadingType.DELAY);
     }
 }
